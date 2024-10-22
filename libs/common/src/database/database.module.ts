defd92bdbb302dc1a98df1c16eac323e462f82f5
mohammedAK1991 <mohmed.ak.17@gmail.com>
2024-10-22 13:17:18 +0200

    feat: Add ConfigModule and DatabaseModule to common library
    This commit adds the ConfigModule and DatabaseModule to the common library in order to provide configuration management and database connectivity functionality. The ConfigModule is responsible for loading and validating environment variables, while the DatabaseModule sets up the connection to the MongoDB database using the Mongoose module. These modules will be used by other components in the application.
    
    Refs: #123

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async () => ({
+        uri: process.env.MONGODB_URI,
+      }),
+    }),
+  ],
+})
 export class DatabaseModule {}
